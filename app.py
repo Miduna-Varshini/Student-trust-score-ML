e4fda63b48e9dfcd4e32f3b8552b3d624121e40c
M.A.Miduna Varshini <mamidunavarshini369@gmail.com>
2025-12-24 14:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ st.markdown(
 # ================= LOAD MODEL =================
 @st.cache_resource
 def load_models():
-    model = joblib.load("/content/drive/MyDrive/PROJRCTS ( ML)/ML code/isolation_forest.pkl")
-    scaler = joblib.load("/content/drive/MyDrive/PROJRCTS ( ML)/ML code/scaler(2).pkl")
+    model = joblib.load("isolation_forest.pkl")
+    scaler = joblib.load("scaler(2).pkl")
     return model, scaler
 
 model, scaler = load_models()
